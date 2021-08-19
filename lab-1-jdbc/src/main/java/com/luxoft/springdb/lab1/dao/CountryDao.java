5fcf1143ec9211e2dfd5fe3623a7852bf659aaa8
Ruslan <r.merdeev@haulmont.com>
2021-08-19 11:06:30 +0400

    Injected the NamedParameterJdbcTemplate to Bean "countryDao" Made the JdbcTest runnable

--- lab-1-jdbc/src/main/java/com/luxoft/springdb/lab1/dao/CountryDao.java
+++ lab-1-jdbc/src/main/java/com/luxoft/springdb/lab1/dao/CountryDao.java
@@ -4,22 +4,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.luxoft.springdb.lab1.model.Country;
 
 public class CountryDao {
-	private static final String LOAD_COUNTRIES_SQL = "insert into country (name, code_name) values (:name, :code_name)";
-
-	private static final String GET_ALL_COUNTRIES_SQL = "select * from country";
-	private static final String GET_COUNTRIES_BY_NAME_SQL = "select * from country where name like :name";
-	private static final String GET_COUNTRY_BY_NAME_SQL = "select * from country where name = '";
-	private static final String GET_COUNTRY_BY_CODE_NAME_SQL = "select * from country where code_name = '";
 
 	private static final String UPDATE_COUNTRY_NAME_SQL_1 = "update country SET name='";
 	private static final String UPDATE_COUNTRY_NAME_SQL_2 = " where code_name='";
@@ -40,14 +31,14 @@ public class CountryDao {
 	}
 
 	public List<Country> getCountryList() {
-		return namedJdbc.query(GET_ALL_COUNTRIES_SQL,
+		return namedJdbc.query("select * from country",
 				COUNTRY_ROW_MAPPER);
 	}
 
 	public List<Country> getCountryListStartWith(String name) {
 		SqlParameterSource sqlParameterSource = new MapSqlParameterSource(
 				"name", name + "%");
-		return namedJdbc.query(GET_COUNTRIES_BY_NAME_SQL,
+		return namedJdbc.query("select * from country where name like :name",
 				sqlParameterSource, COUNTRY_ROW_MAPPER);
 	}
 
@@ -61,23 +52,18 @@ public class CountryDao {
 			map.clear();
 			map.put("name", countryData[0]);
 			map.put("code_name", countryData[1]);
-			String sql = LOAD_COUNTRIES_SQL + "('" + countryData[0] + "', '"
-					+ countryData[1] + "');";
-//			System.out.println(sql);
-//			namedJdbc.query(sql, map, COUNTRY_ROW_MAPPER);
+			namedJdbc.update("insert into country (name, code_name) values (:name, :code_name)", map);
 		}
 	}
 
 	public Country getCountryByCodeName(String codeName) {
-		String sql = GET_COUNTRY_BY_CODE_NAME_SQL + codeName + "'";
-//		System.out.println(sql);
-
+		String sql = "select * from country where code_name = '" + codeName + "'";
 		return namedJdbc.query(sql, COUNTRY_ROW_MAPPER).get(0);
 	}
 
 	public Country getCountryByName(String name)
 			throws CountryNotFoundException {
-		List<Country> countryList = namedJdbc.query(GET_COUNTRY_BY_NAME_SQL
+		List<Country> countryList = namedJdbc.query("select * from country where name = '"
 				+ name + "'", COUNTRY_ROW_MAPPER);
 		if (countryList.isEmpty()) {
 			throw new CountryNotFoundException();
