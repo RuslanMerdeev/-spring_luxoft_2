ac23d3de274a2cb5231f568fd8c805e29c00d4a6
Ruslan <r.merdeev@haulmont.com>
2021-08-19 12:12:07 +0400

    Made the testCountryListStartsWithA and testCountryChange runnable

--- lab-1-jdbc/src/main/java/com/luxoft/springdb/lab1/dao/CountryDao.java
+++ lab-1-jdbc/src/main/java/com/luxoft/springdb/lab1/dao/CountryDao.java
@@ -2,7 +2,6 @@ package com.luxoft.springdb.lab1.dao;
 
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,62 +11,70 @@ import com.luxoft.springdb.lab1.model.Country;
 
 public class CountryDao {
 
-	private static final String UPDATE_COUNTRY_NAME_SQL_1 = "update country SET name='";
-	private static final String UPDATE_COUNTRY_NAME_SQL_2 = " where code_name='";
+    public static final String[][] COUNTRY_INIT_DATA = {{"Australia", "AU"},
+            {"Canada", "CA"}, {"France", "FR"}, {"Hong Kong", "HK"},
+            {"Iceland", "IC"}, {"Japan", "JP"}, {"Nepal", "NP"},
+            {"Russian Federation", "RU"}, {"Sweden", "SE"},
+            {"Switzerland", "CH"}, {"United Kingdom", "GB"},
+            {"United States", "US"}};
 
-	public static final String[][] COUNTRY_INIT_DATA = { { "Australia", "AU" },
-			{ "Canada", "CA" }, { "France", "FR" }, { "Hong Kong", "HK" },
-			{ "Iceland", "IC" }, { "Japan", "JP" }, { "Nepal", "NP" },
-			{ "Russian Federation", "RU" }, { "Sweden", "SE" },
-			{ "Switzerland", "CH" }, { "United Kingdom", "GB" },
-			{ "United States", "US" } };
+    private static final CountryRowMapper COUNTRY_ROW_MAPPER = new CountryRowMapper();
 
-	private static final CountryRowMapper COUNTRY_ROW_MAPPER = new CountryRowMapper();
+    private final NamedParameterJdbcTemplate namedJdbc;
 
-	private final NamedParameterJdbcTemplate namedJdbc;
+    public CountryDao(NamedParameterJdbcTemplate namedJdbc) {
+        this.namedJdbc = namedJdbc;
+    }
 
-	public CountryDao(NamedParameterJdbcTemplate namedJdbc) {
-		this.namedJdbc = namedJdbc;
-	}
+    public List<Country> getCountryList() {
+        return namedJdbc.query("select * from country",
+                COUNTRY_ROW_MAPPER);
+    }
 
-	public List<Country> getCountryList() {
-		return namedJdbc.query("select * from country",
-				COUNTRY_ROW_MAPPER);
-	}
+    public List<Country> getCountryListStartWith(String name) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource(
+                "name", name + "%");
+        return namedJdbc.query("select * from country where name like :name",
+                sqlParameterSource, COUNTRY_ROW_MAPPER);
+    }
 
-	public List<Country> getCountryListStartWith(String name) {
-		SqlParameterSource sqlParameterSource = new MapSqlParameterSource(
-				"name", name + "%");
-		return namedJdbc.query("select * from country where name like :name",
-				sqlParameterSource, COUNTRY_ROW_MAPPER);
-	}
+    public void updateCountryName(String codeName, String newCountryName) {
+        namedJdbc.update("update country SET name=:name where code_name=:code_name",
+                new HashMap<String, String>() {{
+                    put("name", newCountryName);
+                    put("code_name", codeName);
+                }});
+    }
 
-	public void updateCountryName(String codeName, String newCountryName) {
-		// TODO: implement it
-	}
+    public void loadCountries() {
+        for (String[] countryData : COUNTRY_INIT_DATA) {
+            namedJdbc.update("insert into country (name, code_name) values (:name, :code_name)",
+                    new HashMap<String, String>() {{
+                        put("name", countryData[0]);
+                        put("code_name", countryData[1]);
+                    }});
+        }
+    }
 
-	public void loadCountries() {
-		Map<String, String> map = new HashMap<>();
-		for (String[] countryData : COUNTRY_INIT_DATA) {
-			map.clear();
-			map.put("name", countryData[0]);
-			map.put("code_name", countryData[1]);
-			namedJdbc.update("insert into country (name, code_name) values (:name, :code_name)", map);
-		}
-	}
+    public Country getCountryByCodeName(String codeName) {
+        String sql = "select * from country where code_name = :code_name";
+        return namedJdbc.query(sql,
+                new HashMap<String, String>() {{
+                    put("code_name", codeName);
+                }},
+                COUNTRY_ROW_MAPPER).get(0);
+    }
 
-	public Country getCountryByCodeName(String codeName) {
-		String sql = "select * from country where code_name = '" + codeName + "'";
-		return namedJdbc.query(sql, COUNTRY_ROW_MAPPER).get(0);
-	}
-
-	public Country getCountryByName(String name)
-			throws CountryNotFoundException {
-		List<Country> countryList = namedJdbc.query("select * from country where name = '"
-				+ name + "'", COUNTRY_ROW_MAPPER);
-		if (countryList.isEmpty()) {
-			throw new CountryNotFoundException();
-		}
-		return countryList.get(0);
-	}
+    public Country getCountryByName(String name)
+            throws CountryNotFoundException {
+        List<Country> countryList = namedJdbc.query("select * from country where name = :name",
+                new HashMap<String, String>() {{
+                    put("name", name);
+                }},
+                COUNTRY_ROW_MAPPER);
+        if (countryList.isEmpty()) {
+            throw new CountryNotFoundException();
+        }
+        return countryList.get(0);
+    }
 }
