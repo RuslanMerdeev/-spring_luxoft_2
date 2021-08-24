055205446a812133ad0d49faa597c6c8d882a453
merdeevrz <merdeevrz@haulmont.com>
2021-08-24 10:21:24 +0400

    Made the testSaveCountry and testGtAllCountries runnable

--- lab-2-orm/src/main/java/com/luxoft/springdb/lab2/dao/jpa/CountryJpaDaoImpl.java
+++ lab-2-orm/src/main/java/com/luxoft/springdb/lab2/dao/jpa/CountryJpaDaoImpl.java
@@ -2,23 +2,42 @@ package com.luxoft.springdb.lab2.dao.jpa;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.luxoft.springdb.lab2.dao.CountryDao;
 import com.luxoft.springdb.lab2.model.Country;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.transaction.Transactional;
+
 @Repository
-public class CountryJpaDaoImpl extends AbstractJpaDao implements CountryDao {
+public class CountryJpaDaoImpl implements CountryDao {
+
+	@Autowired
+	EntityManagerFactory emf;
 
 	@Override
+	@Transactional
 	public void save(Country country) {
-		// TODO: Implement it
+		EntityManager em = emf.createEntityManager();
+		if (em != null) {
+			em.persist(country);
+			em.close();
+		}
 	}
 
 	@Override
+	@Transactional
 	public List<Country> getAllCountries() {
-		// TODO: Implement it
-		return null;
+		List<Country> countryList = null;
+		EntityManager em = emf.createEntityManager();
+		if (em != null) {
+			countryList = em.createQuery("select e from Country e", Country.class).getResultList();
+			em.close();
+		}
+		return countryList;
 	}
 
 	@Override
